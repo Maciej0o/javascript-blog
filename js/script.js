a1915325bb027e8deaf3ae7b53dafaf120d4675c
Maciej Smolarczyk <smolarczyk.maciek@gmail.com>
2019-12-14 15:20:51 +0100

    Add class active to article

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 });*/
 
 const titleClickHandler = function(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
   console.log('EVENT:', event);
@@ -20,7 +21,7 @@ const titleClickHandler = function(event){
 
   console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
-  
+
   /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts article.active');
@@ -31,9 +32,16 @@ const titleClickHandler = function(event){
   }
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector =  clickedElement.getAttribute("href");
+  console.log('articleSelector', articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('targetArticle', targetArticle);
+
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
